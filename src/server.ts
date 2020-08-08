753fde7364a55d9c15a5184745e59a7cfc3124a3
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-07 22:52:41 -0300

    feat: configure db

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) =>
-  response.json({ message: 'kk eae men' }),
-);
+app.get('/', (request, response) => response.json({ message: 'kk eae men' }));
 
 app.listen(3333);
